991a0e4170a33cb6c67ebc048374abbc0f67d4ab
Maria <mvalenciacuberos@gmail.com>
2023-11-23 14:13:49 +0100

    Change uri back

--- util/apolloClient.ts
+++ util/apolloClient.ts
@@ -5,13 +5,11 @@ import { headers } from 'next/headers';
 export const { getClient } = registerApolloClient(() => {
   // Local GraphQL server API Link
   const localLink = new HttpLink({
-    // uri: `${process.env.NEXT_PUBLIC_BASE_URL}/api/graphql`,
+    uri: `${process.env.NEXT_PUBLIC_BASE_URL}/api/graphql`,
     // uri: 'http://localhost:3000/api/graphql',
     // uri: 'https://final-project-upleveled-sage.fly.dev/api/graphql',
-    uri: "https://final-project-uplevled-sage.vercel.app/api/graphql",
-
-
-   // credentials: 'same-origin',
+    // uri: "https://final-project-uplevled-sage.vercel.app/api/graphql",
+    credentials: 'same-origin',
   });
 
   headers();
